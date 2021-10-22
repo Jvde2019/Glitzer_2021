2cdf0cf990f6fd897c2dc8e7122cdaae7aaf2990
Jvde2019 <57542735+Jvde2019@users.noreply.github.com>
2021-10-22 21:25:26 +0200

    Create Glitzer_2021.h

---
+++ Glitzer_2021.h
@@ -0,0 +1,28 @@
+#ifndef NEWFILE_H
+#define	NEWFILE_H
+/*################################################################
+ * 
+ * File:   Glitzer_2021.h
+ * Author: JM
+ *
+ * Created on 22.Oct 2021, 20:23
+ */
+// PIC16LF628A Configuration Bit Settings
+
+// CONFIG
+#pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSC oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB4/PGM pin has digital I/O function, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+
+#endif
